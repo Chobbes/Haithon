3f269c950f822308073a80ad7feda4cc43a5ea28
Calvin Beck <hobbes@ualberta.ca>
2014-12-28 17:20:14 -0700

    Cabal init and nix.

---
+++ shell.nix
@@ -0,0 +1,19 @@
+{ pkgs ? (import <nixpkgs> {})
+, haskellPackages ? pkgs.haskellPackages_ghc783
+}:
+
+haskellPackages.cabal.mkDerivation (self: {
+  pname = "Haithon";
+  version = "0.1.0.0";
+  src = ./.;
+  isLibrary = true;
+  isExecutable = false;
+  buildDepends = with haskellPackages; [ parsec ];
+  buildTools = with haskellPackages; [ cabalInstall ];
+  meta = {
+    homepage = "https://github.com/Chobbes/Haithon";
+    description = "A Haskell parser for a subset of the Python language. The intention of this project is two-fold! Firstly, I wish to try out Parsec's handling of indentation sensitive languages, and secondly I want to experiment with using this as some course material!";
+    license = self.stdenv.lib.licenses.mit;
+    platforms = self.ghc.meta.platforms;
+  };
+})
